efdf467f6af4a8791e227b20c19992fff290cb4f
edankap <edankap20@gmail.com>
2023-04-03 13:05:15 +0200

    Lab Done

--- src/components/BackgroundColorChanger.vue
+++ src/components/BackgroundColorChanger.vue
@@ -1,11 +1,11 @@
 <template lang="">
-    <div :class="backgroundColor">
+    <div :class="[backgroundColor]">
         <button @click="changeColor">Change Color</button>
         <h1>This is a box with a dynamic background color!</h1>
     </div>
 </template>
 
-<script >
+<script>
 import { ref } from 'vue'
 export default {
      data() {
@@ -15,20 +15,16 @@ export default {
      },
     methods: {
         changeColor (){
-            if (this.backgroundColor === 'red') {
-                this.backgroundColor = 'blue'
-            } else {
-                this.backgroundColor = 'red'
-            }
+            this.backgroundColor = this.backgroundColor === 'red' ? 'blue' : 'red';
         }
     },
 }
 </script>
 
-<style lang="">
+<style lang="css">
    .red {
     background-color: red
-   } 
+   }
    .blue {
     background-color: blue
    }
